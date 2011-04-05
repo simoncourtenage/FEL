a324f5d924aca8ae1e602e2d37d5830d3e756506
Simon Courtenage <courtenage@gmail.com>
2011-04-05 17:19:51 +0100

    Read from config file using boost::program_options

--- config.cpp
+++ config.cpp
@@ -1,7 +1,20 @@
 #include "config.h"
+#include <stdexcept>
+#include <fstream>
 
-
-Config::Config()
+Config::Config(int argc,char** argv)
 {
-
+  po::store(po::parse_command_line(argc,argv,options),vars);
+  po::notify(vars);
+  std::string fopt("cfg");
+  std::string configfilename = instance_->get(fopt);
+  if (configfilename.size() < 1) {
+    configfilename = "fel.cfg";
+  }
+  std::ifstream fs(configfilename.c_str());
+  if (!fs) {
+    throw std::runtime_error("unable to find config file");
+  }
+  po::store(po::parse_config_file(fs,options,true),vars);
+  po::notify(vars);
 }
